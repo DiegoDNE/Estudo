a14dede1b392a4c93cd3b99c413015f38076e700
DiegoDNE <diegodne93@gmail.com>
2024-08-20 00:09:34 -0300

    Update Desafio27.py
    Exercicio otimizado

--- Scripts Python/Desafio27.py
+++ Scripts Python/Desafio27.py
@@ -5,7 +5,7 @@
 #primeiro = Ana
 #último = Souza
 
-nome = input('Digite o nome completo: ')
+'''nome = str(input('Digite o nome completo: ')).strip()
 
 primeiro_nome = nome.split()[0]
 ultimo_nome = nome.split()[-1]
@@ -13,4 +13,18 @@ ultimo_nome = nome.split()[-1]
 print('\n'+'-'*45)
 print('\nO primeiro nome é: {}'.format(primeiro_nome))
 print('\nO último nome é: {}'.format(ultimo_nome))
+print('\n'+'-'*45)'''
+
+#Código otimizado, pra armazenar o nome numa única lista, poupando processamento e ajudando pra futuras expanções de código/manuntenção.
+
+
+nome = str(input('Digite o nome completo: ')).strip()
+
+nomelista = nome.split()
+
 print('\n'+'-'*45)
+print('\nO primeiro nome é: {}'.format(nomelista[0]))
+print('\nO último nome é: {}'.format(nomelista[-1]))
+print('\n'+'-'*45)
+
+#Correção: Correta
